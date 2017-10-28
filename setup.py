ec616aa1a63cd1080d8a24dbd30d473bfa26b588
Tyler Wince <tyler@myndshft.com>
2017-10-28 17:34:16 -0600

    copy and pasted too much for the setup.py

--- setup.py
+++ setup.py
@@ -32,12 +32,6 @@ here = os.path.abspath(os.path.dirname(__file__))
 with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = "\n" + f.read()
 
-# Load the package"s __version__.py module as a dictionary.
-about = {}
-with open(os.path.join(here, NAME, "__version__.py")) as f:
-    exec(f.read(), about)
-
-
 class UploadCommand(Command):
     """Support setup.py upload."""
 
