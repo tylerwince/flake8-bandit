c4c7b63f6275398b0d380d0023010d94a02a8880
Andy Shapiro <shapiromatron@gmail.com>
2020-08-01 14:30:46 -0400

    support all options in .bandit

--- setup.py
+++ setup.py
@@ -113,7 +113,6 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
         "Topic :: Security",
         "Topic :: Software Development :: Libraries :: Python Modules",
@@ -121,4 +120,5 @@ setup(
     ],
     # $ setup.py publish support.
     cmdclass={"upload": UploadCommand},
+    python_requires=">=3.6",
 )
