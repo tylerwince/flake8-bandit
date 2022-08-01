209349898fd7e99d55e978bc0a3eb925a23e41fa
Laurent Sorber <laurent.sorber@gmail.com>
2022-08-02 00:30:11 +0200

    fix: refactor from ConfigFileFinder to load_config

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ AUTHOR = "Tyler Wince"
 VERSION = get_version()
 
 # What packages are required for this module to be executed?
-REQUIRED = ["flake8", "bandit>=1.7.3", "flake8-polyfill", "pycodestyle"]
+REQUIRED = ["flake8>=5.0.0", "bandit>=1.7.3", "flake8-polyfill", "pycodestyle"]
 
 # What packages are optional?
 EXTRAS = {
